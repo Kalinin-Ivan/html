40557c928133dc81476c4464872ea903b55eb8a4
Kalinin Ivan <kalinin.ivan.and@mail.ru>
2020-04-25 14:58:32 +0300

    init

---
+++ course/js/my.js
@@ -0,0 +1,40 @@
+var x = 0
+var n = 0
+
+function number1() {
+    n = 1
+}
+
+function number2() {
+    n = 2
+    alter(n)
+}
+
+function number3() {
+    n = 3
+    alter(n)
+}
+
+function number4() {
+    n = 4
+    alter(n)
+}
+
+function number5() {
+    n = 5
+    alter(n)
+}
+
+function number6() {
+    n = 6
+    alter(n)
+}
+
+function number7() {
+    n = 7
+    alter(n)
+}
+
+function f() {
+   alert("Вы зашли на ГЛАВНОЕ МЕНЮ сайта");
+}
